d454ff1a8c7aab0b59e068f214650ca5bb657102
Arsenicu <kubarodak2003@outlook.com>
2026-02-27 19:46:10 +0000

    server file layout

---
+++ server/src/main/java/com/madeinorbit/server/model/Config.java
@@ -0,0 +1,23 @@
+package com.madeinorbit.server.model;
+
+import java.io.FileReader;
+import java.io.IOException;
+import com.google.gson.Gson;
+
+/**
+ *
+ * @author Kuba Rodak
+ */
+public class Config {
+    public String host = "127.0.0.1";
+    public int port = 8080;
+
+    private static final Gson gson = new Gson();
+    
+    //will load host and port fields from the JSON file
+    public static Config loadFromJson(String path) throws IOException {
+        try (FileReader reader = new FileReader(path)) {
+            return gson.fromJson(reader, Config.class);
+        }
+    }
+}
